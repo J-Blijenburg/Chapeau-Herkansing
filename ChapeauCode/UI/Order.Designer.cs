12e7747f2dd5e80a1b7ccb3b8144abbd8430babc
~Jens <jensblijenburg@gmail.com>
2023-04-30 21:11:49 +0200

    update order view

--- ChapeauCode/UI/Order.Designer.cs
+++ ChapeauCode/UI/Order.Designer.cs
@@ -29,7 +29,138 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Order));
+            LblEmployee = new Label();
+            LblTableNumber = new Label();
+            BtnDrinks = new Button();
+            btnDinner = new Button();
+            btnLunch = new Button();
+            pictureBox1 = new PictureBox();
+            BtnPay = new Button();
+            ListViewOrderdItems = new ListView();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            listView1 = new ListView();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
+            // 
+            // LblEmployee
+            // 
+            LblEmployee.AutoEllipsis = true;
+            LblEmployee.BackColor = Color.FromArgb(255, 179, 71);
+            LblEmployee.Font = new Font("Poppins", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            LblEmployee.Location = new Point(303, 14);
+            LblEmployee.Name = "LblEmployee";
+            LblEmployee.Size = new Size(86, 40);
+            LblEmployee.TabIndex = 11;
+            LblEmployee.Text = "Employee";
+            LblEmployee.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // LblTableNumber
+            // 
+            LblTableNumber.AutoSize = true;
+            LblTableNumber.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            LblTableNumber.ForeColor = Color.FromArgb(196, 196, 196);
+            LblTableNumber.Location = new Point(139, 14);
+            LblTableNumber.Margin = new Padding(2, 0, 2, 0);
+            LblTableNumber.Name = "LblTableNumber";
+            LblTableNumber.Size = new Size(153, 33);
+            LblTableNumber.TabIndex = 10;
+            LblTableNumber.Text = "Table #10";
+            // 
+            // BtnDrinks
+            // 
+            BtnDrinks.BackColor = Color.FromArgb(138, 210, 176);
+            BtnDrinks.Location = new Point(300, 83);
+            BtnDrinks.Margin = new Padding(2);
+            BtnDrinks.Name = "BtnDrinks";
+            BtnDrinks.Size = new Size(90, 50);
+            BtnDrinks.TabIndex = 9;
+            BtnDrinks.Text = "Drinks";
+            BtnDrinks.UseVisualStyleBackColor = false;
+            // 
+            // btnDinner
+            // 
+            btnDinner.BackColor = Color.FromArgb(138, 210, 176);
+            btnDinner.Location = new Point(158, 83);
+            btnDinner.Margin = new Padding(2);
+            btnDinner.Name = "btnDinner";
+            btnDinner.Size = new Size(90, 50);
+            btnDinner.TabIndex = 8;
+            btnDinner.Text = "Dinner";
+            btnDinner.UseVisualStyleBackColor = false;
+            // 
+            // btnLunch
+            // 
+            btnLunch.BackColor = Color.FromArgb(138, 210, 176);
+            btnLunch.Location = new Point(16, 83);
+            btnLunch.Margin = new Padding(2);
+            btnLunch.Name = "btnLunch";
+            btnLunch.Size = new Size(90, 50);
+            btnLunch.TabIndex = 7;
+            btnLunch.Text = "Lunch";
+            btnLunch.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(131, 66);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // BtnPay
+            // 
+            BtnPay.BackColor = Color.FromArgb(255, 179, 71);
+            BtnPay.FlatAppearance.BorderColor = Color.Red;
+            BtnPay.FlatAppearance.BorderSize = 20;
+            BtnPay.Font = new Font("Poppins", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BtnPay.Location = new Point(245, 636);
+            BtnPay.Margin = new Padding(2);
+            BtnPay.Name = "BtnPay";
+            BtnPay.Size = new Size(145, 50);
+            BtnPay.TabIndex = 13;
+            BtnPay.Text = "ADD";
+            BtnPay.UseVisualStyleBackColor = false;
+            // 
+            // ListViewOrderdItems
+            // 
+            ListViewOrderdItems.Location = new Point(6, 12);
+            ListViewOrderdItems.Name = "ListViewOrderdItems";
+            ListViewOrderdItems.Size = new Size(362, 179);
+            ListViewOrderdItems.TabIndex = 0;
+            ListViewOrderdItems.UseCompatibleStateImageBehavior = false;
+            ListViewOrderdItems.View = View.Details;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(ListViewOrderdItems);
+            groupBox1.Location = new Point(16, 434);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(374, 197);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(19, 416);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 28);
+            label1.TabIndex = 15;
+            label1.Text = "Order";
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(16, 160);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(373, 220);
+            listView1.TabIndex = 16;
+            listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Order
             // 
@@ -37,14 +168,39 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(398, 697);
+            Controls.Add(listView1);
+            Controls.Add(groupBox1);
+            Controls.Add(BtnPay);
+            Controls.Add(LblEmployee);
+            Controls.Add(LblTableNumber);
+            Controls.Add(BtnDrinks);
+            Controls.Add(btnDinner);
+            Controls.Add(btnLunch);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "Order";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Order";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label LblEmployee;
+        private Label LblTableNumber;
+        private Button BtnDrinks;
+        private Button btnDinner;
+        private Button btnLunch;
+        private PictureBox pictureBox1;
+        private Button BtnPay;
+        private ListView ListViewOrderdItems;
+        private GroupBox groupBox1;
+        private Label label1;
+        private ListView listView1;
     }
 }
--- ChapeauCode/UI/TableOverview.Designer.cs
+++ ChapeauCode/UI/TableOverview.Designer.cs
@@ -30,18 +30,18 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TableOverview));
             pictureBox1 = new PictureBox();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            label1 = new Label();
-            label2 = new Label();
+            btnLunch = new Button();
+            btnDinner = new Button();
+            BtnDrinks = new Button();
+            LblTableNumber = new Label();
+            LblEmployee = new Label();
             groupBox1 = new GroupBox();
             label3 = new Label();
             label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            button4 = new Button();
-            listView1 = new ListView();
+            LblVatPrice = new Label();
+            LblTotalPrice = new Label();
+            BtnAdd = new Button();
+            ListViewOrderdItems = new ListView();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             groupBox1.SuspendLayout();
             SuspendLayout();
@@ -58,66 +58,66 @@
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
             // 
-            // button1
+            // btnLunch
             // 
-            button1.BackColor = Color.FromArgb(138, 210, 176);
-            button1.Location = new Point(16, 83);
-            button1.Margin = new Padding(2);
-            button1.Name = "button1";
-            button1.Size = new Size(90, 50);
-            button1.TabIndex = 1;
-            button1.Text = "Lunch";
-            button1.UseVisualStyleBackColor = false;
+            btnLunch.BackColor = Color.FromArgb(138, 210, 176);
+            btnLunch.Location = new Point(16, 83);
+            btnLunch.Margin = new Padding(2);
+            btnLunch.Name = "btnLunch";
+            btnLunch.Size = new Size(90, 50);
+            btnLunch.TabIndex = 1;
+            btnLunch.Text = "Lunch";
+            btnLunch.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // btnDinner
             // 
-            button2.BackColor = Color.FromArgb(138, 210, 176);
-            button2.Location = new Point(158, 83);
-            button2.Margin = new Padding(2);
-            button2.Name = "button2";
-            button2.Size = new Size(90, 50);
-            button2.TabIndex = 2;
-            button2.Text = "Dinner";
-            button2.UseVisualStyleBackColor = false;
+            btnDinner.BackColor = Color.FromArgb(138, 210, 176);
+            btnDinner.Location = new Point(158, 83);
+            btnDinner.Margin = new Padding(2);
+            btnDinner.Name = "btnDinner";
+            btnDinner.Size = new Size(90, 50);
+            btnDinner.TabIndex = 2;
+            btnDinner.Text = "Dinner";
+            btnDinner.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // BtnDrinks
             // 
-            button3.BackColor = Color.FromArgb(138, 210, 176);
-            button3.Location = new Point(300, 83);
-            button3.Margin = new Padding(2);
-            button3.Name = "button3";
-            button3.Size = new Size(90, 50);
-            button3.TabIndex = 3;
-            button3.Text = "Drinks";
-            button3.UseVisualStyleBackColor = false;
+            BtnDrinks.BackColor = Color.FromArgb(138, 210, 176);
+            BtnDrinks.Location = new Point(300, 83);
+            BtnDrinks.Margin = new Padding(2);
+            BtnDrinks.Name = "BtnDrinks";
+            BtnDrinks.Size = new Size(90, 50);
+            BtnDrinks.TabIndex = 3;
+            BtnDrinks.Text = "Drinks";
+            BtnDrinks.UseVisualStyleBackColor = false;
             // 
-            // label1
+            // LblTableNumber
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ForeColor = Color.FromArgb(196, 196, 196);
-            label1.Location = new Point(139, 14);
-            label1.Margin = new Padding(2, 0, 2, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(153, 33);
-            label1.TabIndex = 4;
-            label1.Text = "Table #10";
+            LblTableNumber.AutoSize = true;
+            LblTableNumber.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            LblTableNumber.ForeColor = Color.FromArgb(196, 196, 196);
+            LblTableNumber.Location = new Point(139, 14);
+            LblTableNumber.Margin = new Padding(2, 0, 2, 0);
+            LblTableNumber.Name = "LblTableNumber";
+            LblTableNumber.Size = new Size(153, 33);
+            LblTableNumber.TabIndex = 4;
+            LblTableNumber.Text = "Table #10";
             // 
-            // label2
+            // LblEmployee
             // 
-            label2.AutoEllipsis = true;
-            label2.BackColor = Color.FromArgb(255, 179, 71);
-            label2.Font = new Font("Poppins", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(304, 16);
-            label2.Name = "label2";
-            label2.Size = new Size(86, 40);
-            label2.TabIndex = 5;
-            label2.Text = "Employee";
-            label2.TextAlign = ContentAlignment.MiddleCenter;
+            LblEmployee.AutoEllipsis = true;
+            LblEmployee.BackColor = Color.FromArgb(255, 179, 71);
+            LblEmployee.Font = new Font("Poppins", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            LblEmployee.Location = new Point(303, 18);
+            LblEmployee.Name = "LblEmployee";
+            LblEmployee.Size = new Size(86, 40);
+            LblEmployee.TabIndex = 5;
+            LblEmployee.Text = "Employee";
+            LblEmployee.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(listView1);
+            groupBox1.Controls.Add(ListViewOrderdItems);
             groupBox1.Location = new Point(16, 164);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(374, 369);
@@ -144,50 +144,50 @@
             label4.TabIndex = 8;
             label4.Text = "VAT:";
             // 
-            // label5
+            // LblVatPrice
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Poppins", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(330, 595);
-            label5.Name = "label5";
-            label5.Size = new Size(60, 28);
-            label5.TabIndex = 10;
-            label5.Text = "___,-";
-            label5.TextAlign = ContentAlignment.MiddleRight;
+            LblVatPrice.AutoSize = true;
+            LblVatPrice.Font = new Font("Poppins", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            LblVatPrice.Location = new Point(330, 595);
+            LblVatPrice.Name = "LblVatPrice";
+            LblVatPrice.Size = new Size(60, 28);
+            LblVatPrice.TabIndex = 10;
+            LblVatPrice.Text = "___,-";
+            LblVatPrice.TextAlign = ContentAlignment.MiddleRight;
             // 
-            // label6
+            // LblTotalPrice
             // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Poppins", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label6.Location = new Point(330, 558);
-            label6.Name = "label6";
-            label6.Size = new Size(60, 28);
-            label6.TabIndex = 9;
-            label6.Text = "___,-";
-            label6.TextAlign = ContentAlignment.MiddleRight;
+            LblTotalPrice.AutoSize = true;
+            LblTotalPrice.Font = new Font("Poppins", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            LblTotalPrice.Location = new Point(330, 558);
+            LblTotalPrice.Name = "LblTotalPrice";
+            LblTotalPrice.Size = new Size(60, 28);
+            LblTotalPrice.TabIndex = 9;
+            LblTotalPrice.Text = "___,-";
+            LblTotalPrice.TextAlign = ContentAlignment.MiddleRight;
             // 
-            // button4
+            // BtnAdd
             // 
-            button4.BackColor = Color.FromArgb(255, 179, 71);
-            button4.FlatAppearance.BorderColor = Color.Red;
-            button4.FlatAppearance.BorderSize = 20;
-            button4.Font = new Font("Poppins", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            button4.Location = new Point(245, 636);
-            button4.Margin = new Padding(2);
-            button4.Name = "button4";
-            button4.Size = new Size(145, 50);
-            button4.TabIndex = 11;
-            button4.Text = "Pay";
-            button4.UseVisualStyleBackColor = false;
+            BtnAdd.BackColor = Color.FromArgb(255, 179, 71);
+            BtnAdd.FlatAppearance.BorderColor = Color.Red;
+            BtnAdd.FlatAppearance.BorderSize = 20;
+            BtnAdd.Font = new Font("Poppins", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            BtnAdd.Location = new Point(245, 636);
+            BtnAdd.Margin = new Padding(2);
+            BtnAdd.Name = "BtnAdd";
+            BtnAdd.Size = new Size(145, 50);
+            BtnAdd.TabIndex = 11;
+            BtnAdd.Text = "Pay";
+            BtnAdd.UseVisualStyleBackColor = false;
             // 
-            // listView1
+            // ListViewOrderdItems
             // 
-            listView1.Location = new Point(6, 22);
-            listView1.Name = "listView1";
-            listView1.Size = new Size(362, 334);
-            listView1.TabIndex = 0;
-            listView1.UseCompatibleStateImageBehavior = false;
-            listView1.View = View.Details;
+            ListViewOrderdItems.Location = new Point(6, 22);
+            ListViewOrderdItems.Name = "ListViewOrderdItems";
+            ListViewOrderdItems.Size = new Size(362, 334);
+            ListViewOrderdItems.TabIndex = 0;
+            ListViewOrderdItems.UseCompatibleStateImageBehavior = false;
+            ListViewOrderdItems.View = View.Details;
             // 
             // TableOverview
             // 
@@ -195,17 +195,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(398, 697);
-            Controls.Add(button4);
-            Controls.Add(label5);
-            Controls.Add(label6);
+            Controls.Add(BtnAdd);
+            Controls.Add(LblVatPrice);
+            Controls.Add(LblTotalPrice);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(groupBox1);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(LblEmployee);
+            Controls.Add(LblTableNumber);
+            Controls.Add(BtnDrinks);
+            Controls.Add(btnDinner);
+            Controls.Add(btnLunch);
             Controls.Add(pictureBox1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Margin = new Padding(2);
@@ -221,17 +221,17 @@
         #endregion
 
         private PictureBox pictureBox1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Label label1;
-        private Label label2;
+        private Button btnLunch;
+        private Button btnDinner;
+        private Button BtnDrinks;
+        private Label LblTableNumber;
+        private Label LblEmployee;
         private GroupBox groupBox1;
         private Label label3;
         private Label label4;
-        private Label label5;
-        private Label label6;
-        private Button button4;
-        private ListView listView1;
+        private Label LblVatPrice;
+        private Label LblTotalPrice;
+        private Button BtnAdd;
+        private ListView ListViewOrderdItems;
     }
 }
