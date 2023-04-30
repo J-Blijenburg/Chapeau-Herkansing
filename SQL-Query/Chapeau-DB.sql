ee4296793dd58868963f6169d24196b54b2ebaf6
~Jens <jensblijenburg@gmail.com>
2023-04-30 16:23:59 +0200

    De database kolommen

---
+++ SQL-Query/Chapeau-DB.sql
@@ -0,0 +1,96 @@
+/*The Following sql query's are for creating the tables*/
+
+/*This is the ENUM for the status of a table*/
+CREATE TABLE [TableStatus](
+	TableStatusId int identity(1,1) NOT NULL primary key,
+	Status varchar(20)
+);
+
+CREATE TABLE [Table](
+	TableId int identity(1,1) NOT NULL primary key,
+	Number int,
+	Status int foreign key references TableStatus(TableStatusId)
+);
+
+/*This is the ENUM for which payment is selected*/
+CREATE TABLE [PaymentMethod](
+	PaymentMethodId int identity(1,1) NOT NULL primary key,
+	Method int
+);
+
+CREATE TABLE [Payment](
+	PaymentId int identity(1,1) NOT NULL primary key,
+	IsPaid BIT
+);
+
+/*This is the ENUM to selected the employee role*/
+CREATE TABLE [EmployeeRole](
+	EmployeeRoleId int identity(1,1) NOT NULL primary key,
+	Role VarChar(25)
+);
+
+CREATE TABLE [Employee](
+	EmployeeId int identity(1,1) NOT NULL primary key,
+	FirstName varchar(50),
+	LastName varchar(50),
+	EmployeeNumber int,
+	Password varchar(50),
+	IsActive BIT,
+	RegistrationDate Date,
+	Role int foreign key references EmployeeRole(EmployeeRoleId)
+);
+
+CREATE TABLE [Receipt](
+	ReceiptId int identity(1,1) NOT NULL primary key,
+	ReceiptDateTime Date,
+	Feedback varchar(1000),
+	EmployeeId int foreign key references Employee(EmployeeId),
+	TableId int foreign key references [Table](TableId),
+	LowVatPrice float,
+	HighVatPrice float,
+	TotalPrice float,
+	Tip float,
+	IsHandled BIT
+);
+
+/*This Table is a enum for the status of the order*/
+CREATE TABLE [OrderStatus](
+	OrderStatusId int identity(1,1) NOT NULL primary key,
+	Status varchar(25)
+);
+
+CREATE TABLE [Order](
+	OrderId int identity(1,1) NOT NULL primary key,
+	EmployeeId int,
+	ReceiptId int,
+	OrderDateTime DateTime,
+	Status int foreign key references OrderStatus(OrderStatusId)
+);
+
+
+CREATE TABLE [Category](
+	CategoryId int identity(1,1) NOT NULL primary key,
+	VAT float,
+	MenuCard varchar(25)
+);
+
+CREATE TABLE [MenuItem](
+	MenuItemId int identity(1,1) NOT NULL primary key,
+	Name varchar(25),
+	Stock int,
+	Price float,
+	CategoryId int foreign key references Category(CategoryId)
+)
+
+CREATE TABLE [OrderItem](
+	OrderItemId int identity(1,1) NOT NULL primary key,
+	OrderId int,
+	Comment varchar(1000),
+	MenuItemId int foreign key references MenuItem(MenuItemId),
+	Quantity int
+)
+
+/*First the tables has to be created before they can be filled with information*/
+
+GO
+
