05bf1645eb675476cde4b141825cf66a0cc3add6
insertcoolname <47336104+insertcoolname@users.noreply.github.com>
2023-06-19 12:59:27 +0200

    login weer goed zetten

--- ChapeauCode/UI/Login.cs
+++ ChapeauCode/UI/Login.cs
@@ -19,10 +19,8 @@ namespace UI
         }
         private void loginBtn_Click(object sender, EventArgs e)
         {
-            //string employeeNumber = userNameTextBox.Text;
-            //string password = passwordTextbox.Text;
-            string employeeNumber = "611527";
-            string password = "4321";
+            string employeeNumber = userNameTextBox.Text;
+            string password = passwordTextbox.Text;
 
             try
             {
